a46fb48fdbf55d47f961343d89f9182015fceae1
Abdallah Herzallah <fa_herzallah@esi.dz>
2019-09-12 03:35:29 +0100

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../Models/User')
 
 /* GET users listing. */
 router.get('/twilio', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/register', function(req, res, next) {
+  User.create(req.body).then(data => {   res.send(data);}).catch(err => {res.status(400).send(err);})
 });
 
-router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function(req, res, next) {
+  User.findOne(
+      { phone: req.body.phone , password : req.body.password }
+  ).then(data => {   res.send(data);}).catch(err => {res.status(400).send(err);})
 });
 
 module.exports = router;
